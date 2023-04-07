8425cfc8b14b7e59a0b1ae281e154b614b9002b8
Sławomir Gryboś <slawomir.grybos@groszek.pl>
2023-04-07 08:49:19 +0200

    Porządki

--- DateTools/DateRangeDaysCounter.cpp
+++ DateTools/DateRangeDaysCounter.cpp
@@ -9,7 +9,6 @@ DateRangeDaysCounter::DateRangeDaysCounter(void)
 
 int DateRangeDaysCounter::countNumberOfDaysBetweenTwoDates(const std::time_t& dateFrom, const std::time_t& dateTo)const
 {
-	std::chrono::system_clock::time_point dateFromTP;
 	std::chrono::duration<long, std::ratio<1>> differrenceInSeconds(dateTo - dateFrom);
 	auto differenceInDays = std::chrono::duration_cast<std::chrono::duration<long, std::ratio<DAY_RATIO>>>(differrenceInSeconds);
 
